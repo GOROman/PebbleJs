fc344f36b7f4eaac0c81c902c1e9d2f067de5e14
GOROman <goro@net-kick.com>
2015-07-19 15:34:43 +0900

    日本語コメントを記入

--- src/app.js
+++ src/app.js
@@ -4,18 +4,27 @@
  * This is where you write your app.
  */
 
+// 使用するコンポーネント
 var UI = require('ui');
 var Vector2 = require('vector2');
 
+// カードを生成
 var main = new UI.Card({
+  // タイトル
   title: 'Pebble.js',
+  // アイコン
   icon: 'images/menu_icon.png',
+  // サブタイトル
   subtitle: 'Hello World!',
+  // 本文
   body: 'Press any button.'
 });
 
+
+// カードを表示
 main.show();
 
+// 上ボタンクリック時
 main.on('click', 'up', function(e) {
   var menu = new UI.Menu({
     sections: [{
@@ -36,21 +45,28 @@ main.on('click', 'up', function(e) {
   menu.show();
 });
 
+// selectボタンクリック時
 main.on('click', 'select', function(e) {
   var wind = new UI.Window({
+    // フルスクリーン有効
     fullscreen: true,
   });
+
   var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
+    position:  new Vector2(0, 65),
+    size:      new Vector2(144, 30),
+    font:      'gothic-24-bold',
+    text:      'Text Anywhere!',
     textAlign: 'center'
   });
+
   wind.add(textfield);
+
+  // ウィンドウ表示
   wind.show();
 });
 
+// 下ボタンクリック時
 main.on('click', 'down', function(e) {
   var card = new UI.Card();
   card.title('A Card');
